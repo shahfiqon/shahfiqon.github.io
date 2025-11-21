3867b055370f5440368fc817af31bb57e17c3cca
shahfiqon <jan01789362@gmail.com>
2025-11-21 21:40:06 +0545

    baseurl fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   url: "https://shahfiqon.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/portfolio/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
